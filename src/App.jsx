9d0792c533039a50c6fb6e75f03e56c8bcff26f9
Mukherjee-Rik <rikmukherjee21071999.com>
2026-04-06 11:57:54 +0530

    Fix Netlify build errors useref removed form app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
